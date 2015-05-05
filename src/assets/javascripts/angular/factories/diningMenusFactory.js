f134c287bab23f6067a75c7e51c10f2a7c5543ba
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2015-05-05 16:36:16 -0700

    Add simple factory for retrieving Dining Menus

---
+++ src/assets/javascripts/angular/factories/diningMenusFactory.js
@@ -0,0 +1,28 @@
+(function(angular) {
+  'use strict';
+
+  /**
+   * Dining Menus Factory
+   */
+  angular.module('cancentral.factories').factory('diningLocationsFactory', function(apiService) {
+    var url = '/dummy/json/dining_menus.json';
+
+    var parseMenus = function(xhr) {
+      var data = xhr.data;
+
+      if (!data.dining_menus) {
+        return;
+      }
+      
+      return data;
+    };
+
+    var getMenus = function(options) {
+      return apiService.http.request(options, url).then(parseMenus);
+    };
+
+    return {
+      getMenus: getMenus
+    };
+  })
+}(window.angular));
